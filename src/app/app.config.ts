43040a13a1faa193aa300987965af92d83bc9cc9
Websys <websys.sistemas@gmail.com>
2024-10-22 16:17:44 -0300

    feat: integrado firebase com o projeto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+      provideRouter(routes), 
+      provideFirebaseApp(() => initializeApp({"projectId":"apitestewk","appId":"1:843743757840:web:d6e8f6c4a3dc9ab19c6c95","databaseURL":"https://apitestewk-default-rtdb.firebaseio.com","storageBucket":"apitestewk.appspot.com","apiKey":"AIzaSyBeTXBwHkG8H-S01vcMLJ8fSelgi1mk32o","authDomain":"apitestewk.firebaseapp.com","messagingSenderId":"843743757840","measurementId":"G-CSYWFF6W3R"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
